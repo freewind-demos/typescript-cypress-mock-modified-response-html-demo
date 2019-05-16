a6f053b3df6e7833819f4382369424505b278dbe
freewind <nowindlee@gmail.com>
2019-05-16 16:43:23 +0800

    demo

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-TypeScript Cypress Test Demo
-=========================================
+TypeScript Cypress Mock Modified Response Html Demo
+===================================================
 
-注意点：
+在Cypress中，虽然我们可以通过`cy.route`来设置mock response，但是它只对XHR有效，
+`cy.visit`与`cy.request`都会绕过它。
 
-1. `cypress/plugins/index.js`不能使用`.ts`，所以`webpack.config.js`也不能使用`.ts`
-2. 由于混用了`.js`与`.ts`，所以webpack中要设置`resolve`->`extensions`
-3. `tsconfig.json`中一定要设置`"types": [ "cypress" ]`，否则`cy`等typing找不到
+好在`cy.visit`可以读取本地文件，所以我们可以变通一下：首先使用`cy.request`拿到原始的html，
+把它的内容修改后保存为本地文件，再立刻使用`cy.visit`访问该本地文件，就可以实现对某个http请求返回的html
+进行修改的作用。
+
+详情查看<./cypress/integration/hello_spec.ts>
 
 ```
 npm install
-npm run test:open
+npm run server
 
+npm run test:open
 npm run test:run
 ```
--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,39 +1,27 @@
-import {add} from '../../add'
+let tmpFileId = 0;
+
+function nextTmpFileId() {
+  tmpFileId += 1;
+  return tmpFileId;
+}
+
+function visitModifiedHtml(url: string, modify: (html: string) => string) {
+  cy.request(url).its('body').then(html => {
+    const tmpFile = `tmp/${nextTmpFileId()}.html`;
+    cy.writeFile(tmpFile, modify(html));
+    cy.visit(tmpFile);
+  });
+
+}
+
+const url = 'http://localhost:39483/index.html';
 
 describe('TypeScript', () => {
-  it('works', () => {
-    // note TypeScript definition
-    const x: number = 42
-  })
-
   it('checks shape of an object', () => {
-    const object = {
-      age: 21,
-      name: 'Joe',
-    }
-    expect(object).to.have.all.keys('name', 'age')
+    visitModifiedHtml(url, (html) => {
+      return html.replace(`'cypress'`, `'modified-html'`)
+    });
+
+    cy.get('#main').should('have.text', 'Hello, modified-html!');
   })
-
-  it('uses cy commands', () => {
-    cy.wrap({}).should('deep.eq', {})
-  })
-
-  it('tests our example site', () => {
-    cy.visit('https://example.cypress.io/')
-    cy.get('.home-list')
-      .contains('Querying')
-      .click()
-    cy.get('#query-btn').should('contain', 'Button')
-  })
-
-  // enable once we release updated TypeScript definitions
-  it('has Cypress object type definition', () => {
-    expect(Cypress.version).to.be.a('string')
-  })
-
-
-  it('adds numbers', () => {
-    expect(add(2, 3)).to.equal(5)
-  })
-
 })
